df68a3591ba395ae7a85cc7d780e39df21609991
Guilherme Galarça Dutra <udutra@gmail.com>
2024-02-21 18:41:51 -0300

    ViewModels EditorViewModel Validações Padronização Padronizando erros Padronizando o GetById Padronizando o BadRequest Conclusão do módulo

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using BlogAspNet.Data;
+using BlogAspNet.Extensions;
 using BlogAspNet.Models;
+using BlogAspNet.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,11 +16,11 @@ public class CategoryController: ControllerBase
         try
         {
             var categories = await context.Categories.ToListAsync();
-            return Ok(categories);
+            return Ok(new ResultViewModel<List<Category>>(categories));
         }
-        catch (Exception e)
+        catch
         {
-            return StatusCode(500,"05XE05 - Falha interna no servidor!");
+            return StatusCode(500,new ResultViewModel<List<Category>>("05XE05 - Falha interna no servidor!"));
         }
     }
     
@@ -29,42 +31,55 @@ public class CategoryController: ControllerBase
         {
             var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
 
-            return category != null ? Ok(category) : NotFound();
+            return category != null ? 
+                Ok(new ResultViewModel<Category>(category)) : 
+                NotFound(new ResultViewModel<Category>("Conteúdo não encontrado"));
         }
-        catch (Exception e)
+        catch
         {
-            return StatusCode(500,"05XE04 - Falha interna no servidor!");
+            return StatusCode(500,new ResultViewModel<Category>("05XE04 - Falha interna no servidor!"));
         }
     }
 
     [HttpPost("v1/categories")]
-    public async Task<IActionResult> PostAsync([FromBody] Category model, [FromServices] BlogDataContext context)
+    public async Task<IActionResult> PostAsync([FromBody] EditorCategoryViewModel model, [FromServices] BlogDataContext context)
     {
+
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<Category>(ModelState.GetErrors()));
+        
         try
         {
-            await context.Categories.AddAsync(model);
+            var category = new Category()
+            {
+                Id = 0,
+                Name = model.Name,
+                Slug = model.Slug.ToLower()
+            };
+            
+            await context.Categories.AddAsync(category);
             await context.SaveChangesAsync();
-            return Created($"v1/categories/{model.Id}", model);
+            return Created($"v1/categories/{category.Id}", new ResultViewModel<Category>(category));
         }
         catch (DbUpdateException e)
         {
-            return StatusCode(500,"05XE9 - Não foi possível incluir a categoria!");
+            return StatusCode(500,new ResultViewModel<Category>("05XE9 - Não foi possível incluir a categoria!"));
         }
-        catch (Exception e)
+        catch
         {
-            return StatusCode(500,"05XE10 - Falha interna no servidor!");
+            return StatusCode(500,new ResultViewModel<Category>("05XE10 - Falha interna no servidor!"));
         }
     }
     
     [HttpPut("v1/categories/{id:int}")]
-    public async Task<IActionResult> PutAsync([FromRoute] int id, [FromBody] Category model, [FromServices] BlogDataContext context)
+    public async Task<IActionResult> PutAsync([FromRoute] int id, [FromBody] EditorCategoryViewModel model, [FromServices] BlogDataContext context)
     {
         try
         {
             var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
 
             if (category == null)
-                return NotFound();
+                return NotFound(new ResultViewModel<Category>("Conteúdo não encontrado!"));
 
             category.Name = model.Name;
             category.Slug = model.Slug;
@@ -72,15 +87,15 @@ public class CategoryController: ControllerBase
             context.Categories.Update(category);
             await context.SaveChangesAsync();
 
-            return Ok(model);
+            return Ok(new ResultViewModel<Category>(category));
         }
         catch (DbUpdateException e)
         {
-            return StatusCode(500,"05XE8 - Não foi possível alterar a categoria!");
+            return StatusCode(500,new ResultViewModel<Category>("05XE8 - Não foi possível alterar a categoria!"));
         }
         catch (Exception e)
         {
-            return StatusCode(500,"05XE11 - Falha interna no servidor!");
+            return StatusCode(500,new ResultViewModel<Category>("05XE11 - Falha interna no servidor!"));
         }
     }
     
@@ -92,20 +107,20 @@ public class CategoryController: ControllerBase
             var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
 
             if (category == null)
-                return NotFound();
+                return NotFound(new ResultViewModel<Category>("Conteúdo não encontrado!"));
 
             context.Categories.Remove(category);
             await context.SaveChangesAsync();
 
-            return Ok(category);
+            return Ok(new ResultViewModel<Category>(category));
         }
         catch (DbUpdateException e)
         {
-            return StatusCode(500,"05XE7 - Não foi possível excluir a categoria!");
+            return StatusCode(500,new ResultViewModel<Category>("05XE7 - Não foi possível excluir a categoria!"));
         }
         catch (Exception e)
         {
-            return StatusCode(500,"05XE12 - Falha interna no servidor!");
+            return StatusCode(500,new ResultViewModel<Category>("05XE12 - Falha interna no servidor!"));
         }
     }
 }
---
+++ Extensions/ModelStateExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace BlogAspNet.Extensions;
+
+public static class ModelStateExtensions
+{
+    public static List<string> GetErrors(this ModelStateDictionary modelState)
+    {
+        var result = new List<string>();
+        foreach (var item in modelState.Values)
+            result.AddRange(item.Errors.Select(error => error.ErrorMessage));
+        
+        return result;
+    }
+}
